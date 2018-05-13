4a6cffa6ea90ea06ef182bc3d39d6bfa25f66a53
Marlon <martianworm17@gmail.com>
2018-05-13 12:47:48 +0800

    Variables and Multiple Assignment

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,16 @@
 print("Hello World")
+
+# multiple assignment
+sarah, bob, mike = 16,21,17
+
+print(sarah)
+print(bob)
+print(mike)
+
+# multiple variables same value assignment
+jeff = mark = daniel = neil = 17
+
+print(jeff)
+print(mark)
+print(daniel)
+print(neil)
