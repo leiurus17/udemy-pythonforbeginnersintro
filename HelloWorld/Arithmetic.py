ecc433b2139e9a011960f36d3c71b257ffcc94ad
Marlon <martianworm17@gmail.com>
2018-05-15 21:00:02 +0800

    Arithmetic Operators

--- HelloWorld/Arithmetic.py
+++ HelloWorld/Arithmetic.py
@@ -11,3 +11,16 @@ print("age1 / age2")
 print(age1 / age2)
 print("age1 % age2")
 print(age1 % age2)
+
+sent1 = "Today is a beautiful day"
+firstName = "Marlon"
+lastName = "Monzon"
+
+print(firstName + " " + lastName)
+
+print ("HI" * 10)
+
+sentence = "Marlon was playing basketball.";
+
+# Splicing
+print(sentence[0:6])
