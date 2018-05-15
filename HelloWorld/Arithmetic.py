126133b1f3187dab12f35a75ec96c1ae429c3d24
Marlon <martianworm17@gmail.com>
2018-05-15 20:55:01 +0800

    Arithmetic Operators

---
+++ HelloWorld/Arithmetic.py
@@ -0,0 +1,13 @@
+age1 = 12
+age2 = 18
+
+print("age1 + age2")
+print(age1 + age2)
+print("age1 - age2")
+print(age1 - age2)
+print("age1 * age2")
+print(age1 * age2)
+print("age1 / age2")
+print(age1 / age2)
+print("age1 % age2")
+print(age1 % age2)
