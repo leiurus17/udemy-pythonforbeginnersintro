40efbd48d1fac2bed9f6883803a7109b641804d3
Marlon <martianworm17@gmail.com>
2018-05-27 17:06:51 +0800

    Introduction to Lists

---
+++ HelloWorld/PlaceHolders.py
@@ -0,0 +1,13 @@
+name = "Jake"
+name += " is 15 years old"
+print(name)
+
+sent = "%s is the president of the United States"
+
+print(sent%"Barrack")
+
+age = 25
+anotherName = "Hands"
+statement = "%s is %d years old."
+
+print(statement%(anotherName, age))
