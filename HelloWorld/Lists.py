0cd8a865ad29a1c12a2fcf687c7df98c4cfe94cf
Marlon <martianworm17@gmail.com>
2018-05-29 21:16:56 +0800

    Introduction to Lists

---
+++ HelloWorld/Lists.py
@@ -0,0 +1,25 @@
+shopList = ["Apples", "Oranges", "Bananas", "Cheese"]
+
+print(shopList)
+
+print(shopList[2])
+
+print(shopList[0:2])
+
+shopList.append("Milk")
+
+print(shopList)
+
+shopList[2] = "Ketchup"
+
+print(shopList)
+
+shopList2 = ["Tuna", "Noodles", "Bread"]
+
+allShopList = shopList + shopList2;
+
+print(allShopList)
+
+print(min(allShopList))
+
+print(max(allShopList))
