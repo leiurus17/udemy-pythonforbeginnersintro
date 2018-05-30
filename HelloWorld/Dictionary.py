ef4d1006a0c3dbddb82090b42f48026912968d46
Marlon <martianworm17@gmail.com>
2018-05-30 21:29:27 +0800

    Dictionaries

---
+++ HelloWorld/Dictionary.py
@@ -0,0 +1,21 @@
+students = {"Bob":12, "Rachel":13, "Emily":15}
+
+print(students)
+
+print(students["Bob"])
+
+# Change a value of a dictionary
+students["Rachel"] = 15
+
+print(students["Rachel"])
+
+# Delete a value in dictionary by a key
+del students["Emily"]
+
+print(students)
+
+# Add to dictionary
+students["Rody"] = 70;
+students["Manny"] = 24;
+
+print(students)
