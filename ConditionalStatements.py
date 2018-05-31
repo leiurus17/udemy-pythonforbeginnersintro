a9c5afc41b98403bb17e252afc0af1b423ef2b5a
Marlon <martianworm17@gmail.com>
2018-05-31 20:46:31 +0800

    Conditional Statements

---
+++ ConditionalStatements.py
@@ -0,0 +1,21 @@
+if (5 > 3):
+    print("Hello")
+
+if (5 < 3):
+    print("World")
+else:
+    print("Was not true")
+
+# You can compare many things
+
+if(3 >= 2):
+    print("3 is greater than or equal to 2")
+
+age = 15
+
+if(age < 13):
+    print("You are young")
+elif(age >= 13 and age <= 19):
+    print("You are a teenager")
+else:
+    print("You are an adult")
