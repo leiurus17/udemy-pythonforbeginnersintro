d56b92d01ff5ac6ff630824d0122ae6b454ff623
Marlon <martianworm17@gmail.com>
2018-06-03 13:37:37 +0800

    While Loops

---
+++ HelloWorld/ForLoops.py
@@ -0,0 +1,18 @@
+list1 = ["Apples", "Bananas", "Cherries"]
+
+tupl1 = (12, 13, 25, 15)
+
+for item in list1:
+    print(item)
+
+for item in tupl1:
+    print(item)
+
+for i in range(0,10):
+    print(i)
+
+for i in range(0,11, 2): # Specify increment factor
+    print(i)
+
+for i in range(0, 51, 5):
+    print(i)
